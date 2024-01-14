cb7e24daf50c698167c3a5875a9befbf4ad4a723
cassiano yasumitsu <cassiano@usuarioincomum.com>
2024-01-15 00:58:49 +0900

    Component: Comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import { ThumbsUp, Trash } from "@phosphor-icons/react/dist/ssr";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://avatars.githubusercontent.com/u/42375242?v=4" />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Cassiano Tadao Yasumitsu</strong>
+              <time
+                title="14 de Janeiro ás 23:55h"
+                dateTime="2024-01-14 23:55:00"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+            <button title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+          <p>Muito bom Parabéns</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
