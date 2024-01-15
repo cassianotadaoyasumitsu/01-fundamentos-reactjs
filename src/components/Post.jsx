9973e81c80df48971d21b9b018d2aa545c7419dc
cassiano yasumitsu <cassiano@usuarioincomum.com>
2024-01-15 23:16:39 +0900

    Comunicacao entre componentes

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -34,6 +34,8 @@ export function Post({ author, publishedAt, content }) {
     setNewCommentText(event.target.value);
   }
 
+  function deleteComment(comment) {}
+
   return (
     <article className={styles.post}>
       <header>
@@ -82,7 +84,13 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map((comment) => {
-          return <Comment key={comment} content={comment} />;
+          return (
+            <Comment
+              key={comment}
+              content={comment}
+              onDeleteComment={deleteComment}
+            />
+          );
         })}
       </div>
     </article>
